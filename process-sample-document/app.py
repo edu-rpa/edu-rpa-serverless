e4705d4618c2d6f7d70d031b15a0b65670aa617a
Quang Khánh <khanhnguyenmcu@gmail.com>
2023-12-04 11:02:07 +0700

    Deploy function as docker image

--- process-sample-document/app.py
+++ process-sample-document/app.py
@@ -1,23 +1,33 @@
 import json
 import boto3
+import cv2
 
 from transform import perspective_transform
 
 
 def process_sample_document(event, context):
     # Load the image from the S3 bucket
-    s3 = boto3.resource('s3')
+    s3 = boto3.client('s3')
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     document_key = event['Records'][0]['s3']['object']['key']
-    document_object = s3.get_object(Bucket=bucket_name, Key=document_key)
-    document_bytes = document_object['Body'].read()
+
+    # Set up path
+    tmpkey = document_key.replace('/', '')
+    download_path = '/tmp/{}'.format(tmpkey)
+    upload_path = '/tmp/{}-processed'.format(tmpkey)
+
+    s3.download_file(bucket_name, document_key, download_path)
+    image = cv2.imread(download_path)
 
     # Perform perspective transformation
-    document = perspective_transform(document_bytes)
+    document = perspective_transform(image)
+
+    # Save the processed image to the local filesystem
+    cv2.imwrite(upload_path, document)
 
     # Save the processed image to the S3 bucket
     processed_key = document_key.replace('original', 'processed')
-    s3.put_object(Bucket=bucket_name, Key=processed_key, Body=document)
+    s3.upload_file(upload_path, bucket_name, processed_key)
 
     # Return the processed image URL
     processed_url = f'https://{bucket_name}.s3.amazonaws.com/{processed_key}'
